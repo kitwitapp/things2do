232930367a5f59f5b01ff1a206f310eaf38bcbaa
Roshan Sharma <reprogram46@gmail.com>
2020-08-20 10:35:42 +0530

    feat: Allow saving tasks to local database
    Run a transaction on button press and save task to DB.

--- components/InputBox.tsx
+++ components/InputBox.tsx
@@ -2,27 +2,48 @@ import React, { useState } from 'react';
 import { Text, View } from './Themed';
 import { TextInput, StyleSheet } from 'react-native';
 import * as SQLite from 'expo-sqlite';
+import { TouchableHighlight } from 'react-native-gesture-handler';
 
 const db = SQLite.openDatabase('db.db');
+
+db.transaction(tx => {
+  tx.executeSql(
+    'CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, task TEXT)',
+    []
+  )
+})
 
 export default function InputBox() {
   const [task, addTask] = useState('');
   function onChangeText(value: any) {
     addTask(value)
   }
+  function onPress(task: string) {
+    db.transaction(tx => {
+      tx.executeSql('INSERT INTO `tasks` (`task`) VALUES (?)', [task])
+    })
+  }
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput
         onChangeText={onChangeText}
         placeholder='Add a task...'
         style={styles.input}
       />
-      <Text>You wrote: {task}</Text>
+      <TouchableHighlight
+        onPress={() => onPress(task)}
+        style={styles.button}
+      >
+        <Text>ADD</Text>
+      </TouchableHighlight>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   input: {
     color: '#888',
     width: 200,
@@ -31,5 +52,11 @@ const styles = StyleSheet.create({
     paddingVertical: 6,
     borderBottomWidth: 1,
     borderBottomColor: '#eee',
+  },
+  button: {
+    backgroundColor: '#28f',
+    alignItems: 'center',
+    color: '#fff',
+    padding: 10,
   }
 })
